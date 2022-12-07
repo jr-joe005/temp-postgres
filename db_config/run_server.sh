09064c1f7238bc98b55a1806bcf02d893fc1bf3f
jiangrui <jr_joe@foxmail.com>
2022-12-07 18:28:27 +0800

    20221207-04

--- db_config/run_server.sh
+++ db_config/run_server.sh
@@ -1,10 +1,10 @@
 docker stop postgres-db-server
 docker rm postgres-db-server
-docker-compose up
-#docker-compose up -d
-#sleep 10
-#docker exec -it postgres-db-server bash /data/postgresql/init_server.sh
-#docker stop postgres-db-server
-#sleep 5
-#docker start postgres-db-server
-#docker-compose logs -f
+#docker-compose up
+docker-compose up -d
+sleep 10
+docker exec -it postgres-db-server bash /data/postgresql/init_server.sh
+docker stop postgres-db-server
+sleep 5
+docker start postgres-db-server
+docker-compose logs -f
