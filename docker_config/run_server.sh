074ad52cf520407fafb1e1d2aaaa1135f0569b52
jiangrui <jr_joe@foxmail.com>
2022-12-08 15:39:57 +0800

    20221208-02

--- docker_config/run_server.sh
+++ docker_config/run_server.sh
@@ -1,8 +1,11 @@
 docker stop postgres-db-server
+docker stop vault-server
 docker rm postgres-db-server
+docker rm vault-server
+
 #docker-compose up
 docker-compose up -d
-sleep 10
+sleep 5
 docker exec -it postgres-db-server bash /data/postgresql/init_server.sh
 docker stop postgres-db-server
 sleep 5
